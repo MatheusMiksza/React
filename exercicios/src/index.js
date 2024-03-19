99c007f78bfd1aeadb658442241da5f15b1df014
Matheus Miksza <mathmiksza@gmail.com>
2024-03-18 21:13:42 -0300

    Primeiro componente

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'// conversa direro com o dom da pagina
+import Primeiro from './componentes/Primeiro';
 
 const elementoJSX = <h1>React 2</h1>; 
-ReactDOM.render(elementoJSX, document.getElementById('root'))
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 
-//
