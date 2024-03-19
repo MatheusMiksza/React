3377a18026040befae9d6e9e40f6506fd761bafb
Matheus Miksza <mathmiksza@gmail.com>
2024-03-18 21:07:37 -0300

    Aula 01

---
+++ exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'// conversa direro com o dom da pagina
+
+const elementoJSX = <h1>React 2</h1>; 
+ReactDOM.render(elementoJSX, document.getElementById('root'))
+
+//
