d85fc21c9aba3207dbd8e51c0f85d27f87c6e176
Matheus Miksza <mathmiksza@gmail.com>
2024-03-18 21:35:32 -0300

    Aula 3 export

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'// conversa direro com o dom da pagina
 
 //import Primeiro from './componentes/Primeiro';
-import BomDia from './componentes/BomDia';
+//import BomDia from './componentes/BomDia';
+//import { BoaNoite,BoaTarde } from './componentes/Multiplus'
+import Multiplus,{BoaNoite} from './componentes/Multiplus'
+
 //const elementoJSX = <h1>React 2</h1>; 
-ReactDOM.render(<BomDia nome='Matheus' idade='28'/>, document.getElementById('root'))
+
+ReactDOM.render(
+<div>
+<BoaNoite nome='Matheus' idade='28'/>
+<Multiplus.BoaTarde nome='Mysma' idade='28'/>
+</div> , document.getElementById('root'))
 
